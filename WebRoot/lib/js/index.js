1a6d64bf9c1eb65d69d0ee8437fb52ed5ca16aa5
huzongyao <hzy3774@gmail.com>
2017-01-11 22:18:40 +0800

    添加主页

---
+++ WebRoot/lib/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    //轮播初始化
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 3000,
+        pagination: '.swiper-index-pagination',
+        paginationClickable: true
+    });
+});
