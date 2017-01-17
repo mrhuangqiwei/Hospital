47fc2a66b8674274504f948affe9a0a009f4aeaa
huzongyao <hzy3774@gmail.com>
2017-01-17 22:11:47 +0800

    显示科室列表

--- WebRoot/js/class_list.js
+++ WebRoot/js/class_list.js
@@ -2,12 +2,11 @@ new Vue({
     el: '#app',
     data: {todos: []},
     beforeCreate: function () {
-        var url = "https://api.github.com/events";
+        var url = "Ksbmservlet";
         var _self = this;
         $.get(url, function (data, status) {
             if (status === 'success') {
-                _self.todos = data;
-                console.log(data)
+                _self.todos = eval(data);
             } else {
                 alert("请求失败！");
             }
