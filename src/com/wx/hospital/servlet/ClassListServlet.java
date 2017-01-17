2b610888ae1ea8e3fec4826fa07af587807da268
huzongyao <hzy3774@gmail.com>
2017-01-17 18:01:36 +0800

    上传页面

--- src/com/wx/hospital/servlet/ClassListServlet.java
+++ src/com/wx/hospital/servlet/ClassListServlet.java
@@ -14,7 +14,7 @@ public class ClassListServlet extends BaseServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("class_list.html").forward(req, resp);
     }
 
     @Override
