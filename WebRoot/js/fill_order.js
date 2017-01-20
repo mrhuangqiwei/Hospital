48f7b6e2f68698f893e81cd6400910e3e671ab22
huzongyao <hzy3774@gmail.com>
2017-01-20 10:46:05 +0800

    显示预约单

---
+++ WebRoot/js/fill_order.js
@@ -0,0 +1,18 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    $scope.doc = {};
+    $scope.tabs = [];
+    $scope.init = function () {
+        $scope.doc = JSON.parse($.cookie('operate_doc'));
+        if ($scope.doc) {
+            $scope.tabs = [
+                ['预约科室', $scope.doc.ksmc],
+                ['挂号医生', $scope.doc.czyxm],
+                ['科室位置', $scope.doc.mzsbdd],
+                ['预约时间', $scope.doc.xq],
+                ['上班信息', $scope.doc.zcmc]
+            ]
+        }
+    };
+    $scope.init();
+});
