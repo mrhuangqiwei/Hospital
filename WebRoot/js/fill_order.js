5f68b3b4b2a091c95276665539e69b5942a5828f
huzongyao <hzy3774@gmail.com>
2017-02-16 18:54:48 +0800

    微信Jsapi支付

--- WebRoot/js/fill_order.js
+++ WebRoot/js/fill_order.js
@@ -1,3 +1,8 @@
+var openId = $.cookie('wx_openid');
+if (!openId) {
+    window.location.href = 'homepage';
+}
+
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.doc = {};
@@ -16,12 +21,6 @@ app.controller('myCtrl', function ($scope, $http) {
                 ['上班信息', $scope.doc.zcmc],
                 ['挂号费用', $scope.doc.ghfy + '元']
             ];
-            var openId = $.cookie('wx_openid');
-            if (!openId) {
-                window.location.href = 'homepage';
-                return;
-            }
-            //var url = "GetFriendinfo?openid=" + openId;
             var url = "http://219.141.78.173/Hospital/GetFriendinfo?openid=" + openId;
             $http.get(url)
                 .success(function (data) {
@@ -32,21 +31,54 @@ app.controller('myCtrl', function ($scope, $http) {
                 });
         }
     };
-    $scope.init();
 
-    $scope.submitOrder = function () {
+    $scope.getParams = function () {
+        var url = "fillorder";
         $http({
             method: 'post',
-            url: 'fillorder',
+            url: url,
+            params: {type: 'get_jsapi_params'}
+        }).success(function (req) {
+            $scope.jsapiTicket = req.jsapiTicket;
+            wx.config({
+                debug: true,
+                appId: req.appid,
+                timestamp: req.timeStamp,
+                nonceStr: req.nonceStr,
+                signature: req.signature,
+                jsApiList: ['chooseWXPay']
+            });
+        })
+    };
+
+    $scope.submitOrder = function () {
+        var url = "fillorder";
+        $http({
+            method: 'post',
+            url: url,
             params: {
                 type: 'weixin_pay',
                 openid: $.cookie('wx_openid'),
                 user_name: $scope.myValue.brxm,
                 user_id: $scope.myValue.sfzh,
-                doc_name: $scope.doc.czyxm
+                doc_name: $scope.doc.czyxm,
+                money: $scope.doc.ghfy
             }
         }).success(function (req) {
-            console.log(req);
+            wx.chooseWXPay({
+                timestamp: 0,
+                nonceStr: req.nonce_str,
+                package: req.prepay_id,
+                signType: 'MD5',
+                paySign: req.sign,
+                success: function (res) {
+                    window.location.href = 'homepage';
+                }
+            });
         })
-    }
+
+    };
+
+    $scope.getParams();
+    $scope.init();
 });
--- src/com/wx/hospital/servlet/OrderServlet.java
+++ src/com/wx/hospital/servlet/OrderServlet.java
@@ -1,17 +1,25 @@
 package com.wx.hospital.servlet;
 
+import com.alibaba.fastjson.JSON;
+import org.sword.wechat4j.jsapi.JsApiManager;
+import org.sword.wechat4j.jsapi.JsApiParam;
 import org.sword.wechat4j.pay.PayManager;
 import org.sword.wechat4j.pay.exception.PayApiException;
 import org.sword.wechat4j.pay.exception.PayBusinessException;
 import org.sword.wechat4j.pay.exception.SignatureException;
 import org.sword.wechat4j.pay.protocol.unifiedorder.UnifiedorderRequest;
 import org.sword.wechat4j.pay.protocol.unifiedorder.UnifiedorderResponse;
+import org.sword.wechat4j.util.RandomStringGenerator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.wx.hospital.HospitalConfig.SERVER_URL;
 
 /**
  * Created by hzy on 1/20/17.
@@ -19,6 +27,8 @@ import java.io.IOException;
  */
 @WebServlet("/fillorder")
 public class OrderServlet extends BaseServlet {
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,18 +42,31 @@ public class OrderServlet extends BaseServlet {
             resp.getWriter().write("error request!");
             return;
         }
-        /***
-         * 前端请求了微信支付的接口，此时需要后端请求微信支付接口完成支付
-         * */
+        /** 微信支付*/
         if (reqType.equals("weixin_pay")) {
-            String openId = req.getParameter("openid");
             UnifiedorderRequest request = new UnifiedorderRequest();
+            String openId = req.getParameter("openid");
             request.setOpenid(openId);
+            request.setBody("挂号费用");
+            String tradeNo = simpleDateFormat.format(new Date()) + openId + RandomStringGenerator.generate(5);
+            request.setOut_trade_no(tradeNo);
+            int totalFee = (int) (Float.parseFloat(req.getParameter("money")) * 100);
+            request.setTotal_fee(totalFee);
+            String spbill_create_ip = req.getRemoteAddr();
+            request.setSpbill_create_ip(spbill_create_ip);
+            request.setTrade_type("JSAPI");
+            String notifyUrl = SERVER_URL + "wcPayNotify";
+            request.setNotify_url(notifyUrl);
             try {
                 UnifiedorderResponse respond = PayManager.unifiedorder(request);
+                resp.getWriter().write(JSON.toJSONString(respond));
             } catch (SignatureException | PayApiException | PayBusinessException e) {
                 e.printStackTrace();
             }
+        } else if (reqType.equals("get_jsapi_params")) {
+            String url = SERVER_URL + "fillorder";
+            JsApiParam params = JsApiManager.signature(url);
+            resp.getWriter().write(JSON.toJSONString(params));
         }
     }
 }
